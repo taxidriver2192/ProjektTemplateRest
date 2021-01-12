e3e8d70c538c2db36d6fdc9afc1ae945b330d09d
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-01-12 16:38:46 +0100

    Update and se if it can push to github? and afther that update the website on azure?

--- RestTemplate/Controllers/ItemController.cs
+++ RestTemplate/Controllers/ItemController.cs
@@ -24,14 +24,14 @@ namespace RestTemplate.Controllers
             new Item(8, "iPhoen 6S+", false, 1500)
         };
 
-        // GET: api/<ItemsController>
+        // GET: api/<ItemController>
         [HttpGet]
         [EnableCors("CorsApi")]
         public IEnumerable<Item> Get()
         {
             return Data;
         }
-        // GET api/<ItemsController>/5
+        // GET api/<ItemController>/5
         [HttpGet]
         [Route("{id}")]
         [ProducesResponseType(200)]
@@ -43,7 +43,7 @@ namespace RestTemplate.Controllers
         }
 
 
-        // GET: api/<ItemsController>
+        // GET: api/<ItemController>
         [HttpGet]
         [Route("Search")]
         public IEnumerable<Item> Get([FromQuery] QueryItems query)
@@ -70,14 +70,14 @@ namespace RestTemplate.Controllers
             return tmpList2;
         }
 
-        // POST api/<ItemsController>
+        // POST api/<ItemController>
         [HttpPost]
         public void Post([FromBody] Item value)
         {
             Data.Add(value);
         }
 
-        // PUT api/<ItemsController>/5
+        // PUT api/<ItemController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] Item value)
         {
@@ -92,7 +92,7 @@ namespace RestTemplate.Controllers
         }
 
 
-        // DELETE api/<ItemsController>/5
+        // DELETE api/<ItemController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
